6f2ae90c1ea7a6c88b4833de2330cde38b6a8954
XiangPan <xiangpan@nyu.edu>
2021-06-15 05:35:45 -0400

    lab1

--- Lab1/README.md
+++
@@ -1,5 +0,0 @@
-# Lab1_Linker
-please ensure g++-9.1 works
-
-make
-./linker <INPUT-FILE>
--- Lab1/submissions/main.cpp
+++ Lab1/submissions/main.cpp
@@ -10,42 +10,6 @@
 #include "tokenizer.hpp"
 using namespace std;
 
-
-
-class Linker 
-{
-    
-    private:
-        void print_symbol_table();
-        void print_memory_map();
-};
-
-
-
-// void Linker::print_symbol_table(vector<string>& def_list, unordered_map<string, bool> md) 
-// void Linker::print_symbol_table() 
-
-// {
-    // cout << "Symbol Table" << endl;
-    // for (string& symbol : deflist) 
-    // {
-    //     cout << symbol << "=" << symbol_table_[symbol];
-    //     if (md[symbol]) {
-    //         cout << " Error: This variable is multiple times defined; first value used";
-    //     }
-    //     cout << endl;
-    // }
-    // cout << endl;
-// template <typename Enumeration>
-// auto as_integer(Enumeration const value)-> typename std::underlying_type<Enumeration>::type
-// {
-//      return static_cast<typename std::underlying_type<Enumeration>::type>(value);
-// }
-
-
-
-
-
 int main(int argc, const char * argv[]) 
 {
     // cout<<argc<<endl;
@@ -61,8 +25,5 @@ int main(int argc, const char * argv[])
         return -1;
     }
     tokenizer.tokenize_pass2();
-    // tokenizer.readSymbol();
-    // string a = "aaa";
-    // isSymbol(a);
     return 0;
 }
