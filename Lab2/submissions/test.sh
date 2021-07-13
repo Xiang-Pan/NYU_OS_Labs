29f5a372875d55bdd4cf026d2d2c70af6b3b28cf
XiangPan <xiangpan@nyu.edu>
2021-07-13 17:17:52 -0400

    Lab2 sub

--- Lab2/submissions/test.sh
+++
@@ -1,5 +0,0 @@
-make clean
-make
-cd lab2_assign
-./runit.sh ../sched_out ../sched
-./gradeit.sh ./refout ../sched_out
--- Lab2/submissions/test_v.sh
+++
@@ -1,5 +0,0 @@
-make clean
-make
-cd lab2_assign
-./runit.sh ../sched_v_out ../sched -v
-./gradeit.sh ./refout_v ../sched_v_out
