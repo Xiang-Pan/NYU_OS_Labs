d84ad4f337fa158a99921b5a83d6d6f6945dee49
XiangPan <xiangpan@nyu.edu>
2021-08-12 20:01:05 -0400

    bug prevent

--- Lab4/test_generated_samples.sh
+++ Lab4/test_generated_samples.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: Xiang Pan
  # @Date: 2021-08-12 18:35:36
- # @LastEditTime: 2021-08-12 19:40:14
+ # @LastEditTime: 2021-08-12 20:00:43
  # @LastEditors: Xiang Pan
  # @Description: 
  # @FilePath: /Lab4/test_generated_samples.sh
@@ -14,9 +14,28 @@ function rand(){
     echo $(($num%$max+$min))
 }
 
-rnd=$(rand 400000 500000)
+function random_float() {
+  local precision=$1
+  local scale=$2
+  local random=$( echo "${RANDOM}/${RANDOM_MAX_INT}*(10^${precision}) + ${RANDOM}/${RANDOM_MAX_INT} " | bc -l )
+  printf "%.${scale}f" ${random}
+    # echo $(($random))
+}
 
-./iomake -t 65536 -i 10 -f 1 > ./generated_samples/random_samples
+num_ios=$(rand 1 9999)
+echo $num_ios
+
+track_num=$(rand 1 1999)
+echo $track_num
+
+lambda=$(seq 0 .01 10 | shuf | head -n1)
+echo $lambda
+
+interarrival_factor=$(seq 0 .01 2 | shuf | head -n1)
+echo $interarrival_factor
+
+./iomake -t $track_num -i $num_ios -L $lambda -f $interarrival_factor > ./generated_samples/random_samples
+echo "./iomake -t $track_num -i $num_ios -L $lambda -f $interarrival_factor > ./generated_samples/random_samples"
 
 SCHED="./iosched"
 OUTDIR="./generated_samples_outs"
@@ -33,9 +52,11 @@ for s in ${SCHEDS}; do
     ${SCHED} -s${s} -v ./generated_samples/${f} > ${OUTDIR}/${f}_${s} 
     ${SCHED_REF} -s${s} -v ./generated_samples/${f} > ${OUTDIR_REF}/${f}_${s} 
     echo "diff ${OUTDIR}/${f}_${s} ${OUTDIR}/${f}_${s}"
-    # diff ${OUTDIR_REF}/${f}_${s} ${OUTDIR}/${f}_${s} > ./generated_samples_diff/${f}_${s}
-    diff ${OUTDIR_REF}/${f}_${s} ${OUTDIR}/${f}_${s} 
+    diff ${OUTDIR_REF}/${f}_${s} ${OUTDIR}/${f}_${s} >> ./generated_samples_diff/${f}_${s}
+    # diff ${OUTDIR_REF}/${f}_${s} ${OUTDIR}/${f}_${s} 
 done
 
-# ./runit.sh  ./iosched_out_v ../iosched -v
-# ./gradeit.sh ./lab4_ref_v ../iosched_out_v
+
+
+# # ./runit.sh  ./iosched_out_v ../iosched -v
+# # ./gradeit.sh ./lab4_ref_v ../iosched_out_v
